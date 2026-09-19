7a54068bccf02c97db90aabc8a27ccc308c80715
anirudh trivedi <anirudhtrivedi5095@gmail.com>
2026-09-20 00:14:50 +0530

    Remove unwanted files and folders from repository

--- onst express = require("express");.js
+++
@@ -1,9 +0,0 @@
-onst express = require("express");
-const app = express();
-app.get("/health-checkup", function (req, res) {
-            //do something with kidneys here
-            const kidneys= req.body.kidneys;
-            const kidneyLength = kidneys.length;
-            res.send("Kidney length is: " + kidneyLength);
-});
-app.listen(3000);
